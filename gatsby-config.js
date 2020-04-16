7aaabf6ef5849470367539b848b7a2fb3860e177
pakjiddat <pakjiddat@gmail.com>
2020-04-16 15:30:42 +0500

    Add Google Analytics tracking code

--- gatsby-config.js
+++ gatsby-config.js
@@ -123,7 +123,7 @@ module.exports = {
     'gatsby-plugin-sharp',
     {
       resolve: 'gatsby-plugin-google-analytics',
-      options: { trackingId: 'UA-134239835-1' },
+      options: { trackingId: 'UA-163786015-1' },
     },
     {
       resolve: 'gatsby-plugin-google-fonts',
